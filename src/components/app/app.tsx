bb03add3ea2c27ca8dbcbd64e289de3c3730f33f
alex <mrpatrickalex@gmail.com>
2025-08-06 16:39:40 +0300

    правки билда.

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,6 +1,6 @@
 // import FavoritesPage from '../../pages/favorites-page/favorites-page';
 // import LoginPage from '../../pages/login-page/login-page';
-import OfferPage from '../../pages/offer-page/offer-page';
+// import OfferPage from '../../pages/offer-page/offer-page';
 import MainPage from '../../pages/main-page/main-page';
 import AppProps from './app.props';
 
