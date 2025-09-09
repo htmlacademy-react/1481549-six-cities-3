ea24b3cb3bd896ccc87bc48b315f8034ea24db1e
alex <mrpatrickalex@gmail.com>
2025-09-09 17:05:49 +0300

    деструктуризация в content.tsx

--- src/pages/offer-page/components/content/content.tsx
+++ src/pages/offer-page/components/content/content.tsx
@@ -1,3 +1,4 @@
+import Premium from '@components/card/premium';
 import Reviews from '../reviews/reviews';
 import Offer from '@models/offer';
 
@@ -6,16 +7,24 @@ type ContentProps = {
 };
 
 export default function Content({ offer }: ContentProps): JSX.Element {
+  const {
+    isPremium,
+    title,
+    rating,
+    bedrooms,
+    maxAdults,
+    price,
+    goods,
+    host: { avatarUrl, name, isPro },
+    description,
+  } = offer;
+
   return (
     <div className="offer__container container">
       <div className="offer__wrapper">
-        {offer.isPremium && (
-          <div className="offer__mark">
-            <span>Premium</span>
-          </div>
-        )}
+        {isPremium && <Premium />}
         <div className="offer__name-wrapper">
-          <h1 className="offer__name">{offer.title}</h1>
+          <h1 className="offer__name">{title}</h1>
           <button className="offer__bookmark-button button" type="button">
             <svg className="offer__bookmark-icon" width="31" height="33">
               <use xlinkHref="#icon-bookmark"></use>
@@ -25,32 +34,27 @@ export default function Content({ offer }: ContentProps): JSX.Element {
         </div>
         <div className="offer__rating rating">
           <div className="offer__stars rating__stars">
-            <span style={{ width: '80%' }}></span>
+            <span style={{ width: `${rating * 20}%` }}></span>
             <span className="visually-hidden">Rating</span>
           </div>
-          <span className="offer__rating-value rating__value">
-            {offer.rating}
-          </span>
+          <span className="offer__rating-value rating__value">{rating}</span>
         </div>
         <ul className="offer__features">
-          {/* <li className="offer__feature offer__feature--entire">
-            {offer.features.entire}
-          </li> */}
           <li className="offer__feature offer__feature--bedrooms">
-            {offer.bedrooms} Bedrooms
+            {bedrooms} Bedrooms
           </li>
           <li className="offer__feature offer__feature--adults">
-            Max {offer.maxAdults} adults
+            Max {maxAdults} adults
           </li>
         </ul>
         <div className="offer__price">
-          <b className="offer__price-value">&euro;{offer.price}</b>
+          <b className="offer__price-value">&euro;{price}</b>
           <span className="offer__price-text">&nbsp;night</span>
         </div>
         <div className="offer__inside">
           <h2 className="offer__inside-title">What&apos;s inside</h2>
           <ul className="offer__inside-list">
-            {offer.goods.map((opt) => (
+            {goods.map((opt) => (
               <li className="offer__inside-item" key={opt}>
                 {opt}
               </li>
@@ -63,17 +67,17 @@ export default function Content({ offer }: ContentProps): JSX.Element {
             <div className="offer__avatar-wrapper offer__avatar-wrapper--pro user__avatar-wrapper">
               <img
                 className="offer__avatar user__avatar"
-                src={offer.host.avatarUrl}
+                src={avatarUrl}
                 width="74"
                 height="74"
                 alt="Host avatar"
               />
             </div>
-            <span className="offer__user-name">{offer.host.name}</span>
-            <span className="offer__user-status">{offer.host.isPro}</span>
+            <span className="offer__user-name">{name}</span>
+            <span className="offer__user-status">{isPro}</span>
           </div>
           <div className="offer__description">
-            <p className="offer__text">{offer.description}</p>
+            <p className="offer__text">{description}</p>
           </div>
         </div>
         <Reviews />
