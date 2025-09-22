9b9334c065a5e9abf7bd9faa2ffd1b75e2aabece
alex <mrpatrickalex@gmail.com>
2025-09-22 14:40:02 +0300

    подключил стили для leaflet

--- src/hooks/useMap.ts
+++ src/hooks/useMap.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState, MutableRefObject, useRef } from 'react';
 import { Map, TileLayer } from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 import City from '@models/city';
 
 function useMap(
